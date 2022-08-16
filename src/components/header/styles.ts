0f495e473dc82cfd6df0ce61309d491045380350
nairo-mudumane <nairomudumane@outlook.com>
2022-08-16 12:10:40 +0200

    responsive header

--- src/components/header/styles.ts
+++ src/components/header/styles.ts
@@ -25,4 +25,44 @@ export const StyledHeader = styled.div`
     font-weight: 500;
     font-size: 3em;
   }
+
+  @media only screen and (min-width: 2560px) {
+    .header {
+      padding: 2rem 0;
+    }
+
+    .logo {
+      width: 6rem;
+    }
+
+    .title {
+      font-size: 4em;
+    }
+  }
+
+  @media only screen and (max-width: 768px) {
+    .title {
+      font-size: 2em;
+    }
+  }
+
+  @media only screen and (max-width: 425px) {
+    .logo {
+      width: 3rem;
+    }
+
+    .title {
+      font-size: 1.5em;
+    }
+  }
+
+  @media only screen and (max-width: 320px) {
+    .logo {
+      display: none;
+    }
+
+    .title {
+      margin: 0 auto;
+    }
+  }
 `;
